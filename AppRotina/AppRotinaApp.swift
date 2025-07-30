30e9c7103039405be5ebd9675eba94360c94268d
Angelo Mendes <angelo030825@gmail.com>
2025-07-30 09:05:48 -0300

    Initial Commit

---
+++ AppRotina/AppRotinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppRotinaApp.swift
+//  AppRotina
+//
+//  Created by Ângelo Mendes on 30/07/25.
+//
+
+import SwiftUI
+
+@main
+struct AppRotinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
